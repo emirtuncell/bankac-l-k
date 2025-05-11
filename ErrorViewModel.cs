578e0254ac1ce27fb2b2f8d0de1a26a15fccdf93
EMİR TUNCEL <160755016+emirtuncell@users.noreply.github.com>
2025-05-11 20:45:39 +0300

    Add files via upload

---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ilk_denemem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ParaTransferController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ilk_denemem.Controllers
+{
+    public class ParaTransferController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
